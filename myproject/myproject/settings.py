98f8d933c40fde057a8882d896fe6a1f0ffedbcf
divya <divyaamulraj5@gmil.com>
2025-04-23 23:21:45 +0530

    white noise error

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -51,7 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoise.Middleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+     
 ]
 
 ROOT_URLCONF = 'myproject.urls'
