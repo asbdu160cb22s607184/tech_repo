60c494607e5332fde73e16661ce7b5e003c65dc8
divya <divyaamulraj5@gmil.com>
2025-04-24 08:21:27 +0530

    remove one mp

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'myproject.myproject.scanner'
+    'myproject.scanner'
 ]
 
 MIDDLEWARE = [
